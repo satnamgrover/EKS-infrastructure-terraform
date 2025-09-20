6e0dd007f8d224b3b1c946c6eb623ce62a294051
satnamgrover <satnamarora35@gmail.com>
2025-09-20 04:37:04 +0000

    eks cluster

---
+++ eks.tf
@@ -0,0 +1,22 @@
+# Create EKS Cluster
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  # Managed Node Group
+  eks_managed_node_groups = {
+    default = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t3.small"]
+    }
+  }
+}
+
